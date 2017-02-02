51a85c27694bd00d27368d47e3caf76061942e4f
Jason Reeder <info@jasonreeder.com>
2017-02-02 15:31:06 -0800

    Rebuild #second_anagram to specs

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -5,11 +5,21 @@ def first_anagram?(word1, word2)
 end
 
 def second_anagram?(word1, word2)
-  return false unless word1.length == word2.length
-  word1.chars.each { |char| word2.delete!(char) }
-  word2.empty?
+  copy1 = word1
+  copy2 = word2
+
+  word1.chars.each do |char1|
+    word2.chars.each do |char2|
+      if char1 == char2
+        copy1.delete!(char1)
+        copy2.delete!(char1)
+      end
+    end
+  end
+
+  copy1.empty? && copy2.empty?
 end
 
 def third_anagram?(word1, word2)
-  
+  word1.chars.sort == word2.chars.sort
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,31 +1,63 @@
 require 'anagrams'
 
-describe '#first_anagram?' do
-  it 'returns true for first_anagrams' do
-    expect(first_anagram?("elvis", "lives")).to be(true)
+context 'with long anagrams' do
+  let(:long1) { 'tobeornottobethatisthequestionwhethertisnoblerinthemindtosuffertheslingsandarrowsofoutrageousfortune' }
+  let(:long2) { 'inoneofthebardsbestthoughtoftragediesourinsistentherohamletqueriesontwofrontsabouthowlifeturnsrotten' }
+
+  describe '#first_anagram' do
+    it 'recognize true anagrams' do
+      expect(first_anagram?(long1, long2)).to be(true)
+    end
   end
 
-  it 'returns false for non-first_anagrams' do
-    expect(first_anagram?("gizmo", "sally")).to be(false)
+  describe '#second_anagram' do
+    it 'recognize true anagrams' do
+      expect(second_anagram?(long1, long2)).to be(true)
+    end
+  end
+
+  describe '#third_anagram' do
+    it 'recognize true anagrams' do
+      expect(third_anagram?(long1, long2)).to be(true)
+    end
   end
 end
 
-describe '#second_anagram?' do
-  it 'returns true for second_anagrams' do
-    expect(second_anagram?("elvis", "lives")).to be(true)
+context 'with short anagrams' do
+  let(:short1) { 'elvis' }
+  let(:short2) { 'lives' }
+
+  let(:short_false1) { 'gizmo' }
+  let(:short_false2) { 'sally' }
+
+  describe '#first_anagram?' do
+    it 'returns true for first_anagrams' do
+      expect(first_anagram?(short1, short2)).to be(true)
+    end
+
+    it 'returns false for non-first_anagrams' do
+      expect(first_anagram?(short_false1, short_false2)).to be(false)
+    end
   end
 
-  it 'returns false for non-second_anagrams' do
-    expect(second_anagram?("gizmo", "sally")).to be(false)
+  describe '#second_anagram?' do
+    it 'returns true for second_anagrams' do
+      expect(second_anagram?(short1, short2)).to be(true)
+    end
+
+    it 'returns false for non-second_anagrams' do
+      expect(second_anagram?(short_false1, short_false2)).to be(false)
+    end
+  end
+
+  describe '#third_anagram?' do
+    it 'returns true for third_anagrams' do
+      expect(third_anagram?(short1, short2)).to be(true)
+    end
+
+    it 'returns false for non-third_anagrams' do
+      expect(third_anagram?(short_false1, short_false2)).to be(false)
+    end
   end
 end
 
-describe '#third_anagram?' do
-  it 'returns true for third_anagrams' do
-    expect(third_anagram?("elvis", "lives")).to be(true)
-  end
-
-  it 'returns false for non-third_anagrams' do
-    expect(third_anagram?("gizmo", "sally")).to be(false)
-  end
-end
