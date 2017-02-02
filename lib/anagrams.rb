9787e7a106320bf4e93ac2d460c536c1b9e91c8d
Jason Reeder <info@jasonreeder.com>
2017-02-02 15:01:41 -0800

    Add code for #second_anagram?

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,7 +1,11 @@
+require 'byebug'
+
 def first_anagram?(word1, word2)
   word1.chars.permutation.include?(word2.chars)
 end
 
 def second_anagram?(word1, word2)
-  
+  return false unless word1.length == word2.length
+  word1.chars.each { |char| word2.delete!(char) }
+  word2.empty?
 end
